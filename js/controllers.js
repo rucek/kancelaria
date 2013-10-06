b0f7a1f25489d0b3f334edcc9201ca6485772ca0
Jacek Kunicki <jacek@kunicki.org>
2013-10-06 23:36:53 +0200

    Disabled links to empty pages

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,10 @@
 function MainCtrl($scope, $location) {
     $scope.menuItems = [
         {path: '/', label: 'Strona główna'},
-        {path: '/o-kancelarii', label: 'O kancelarii'},
+//        {path: '/o-kancelarii', label: 'O kancelarii'},
         {path: '/zespol', label: 'Zespół'},
         {path: '/uslugi', label: 'Usługi'},
-        {path: '/kontakt', label: 'Kontakt'}
+//        {path: '/kontakt', label: 'Kontakt'}
     ];
 
     $scope.$location = $location;
