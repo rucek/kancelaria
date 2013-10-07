7aa6b3cd5dba89e37641b154ffa63b9a6489b95e
Jacek Kunicki <jacek@kunicki.org>
2013-10-07 22:48:23 +0200

    SEO - updated home page URL

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 var officeApp = angular.module('officeApp', [], function ($routeProvider, $locationProvider) {
     $routeProvider.
-        when('/', {templateUrl: 'partials/home.html', controller: MainCtrl}).
+        when('/strona-glowna', {templateUrl: 'partials/home.html', controller: MainCtrl}).
         when('/o-kancelarii', {templateUrl: 'partials/about.html', controller: MainCtrl}).
         when('/zespol', {templateUrl: 'partials/team.html', controller: MainCtrl}).
         when('/uslugi', {templateUrl: 'partials/services.html', controller: MainCtrl}).
         when('/kontakt', {templateUrl: 'partials/contact.html', controller: MainCtrl}).
-        otherwise({redirectTo: '/'});
+        otherwise({redirectTo: '/strona-glowna'});
 
     $locationProvider.hashPrefix('!');
 });
--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@ function MainCtrl($scope, $location) {
     $scope.status = '';
 
     $scope.menuItems = [
-        {path: '/', label: 'Strona główna'},
+        {path: '/strona-glowna', label: 'Strona główna'},
 //        {path: '/o-kancelarii', label: 'O kancelarii'},
         {path: '/zespol', label: 'Zespół'},
         {path: '/uslugi', label: 'Usługi'},
